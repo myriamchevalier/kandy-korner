e29b78749bbf0d068f233b1447efc5341d21028d
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-08-28 16:52:49 -0500

    added ProductList.js and imported it in KandyKorner.js

---
+++ src/products/ProductList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+export const ProductList = () => {
+    const [products, setProducts] = useState([])
+
+    
+    useEffect( () => {
+        fetch("http://localhost:8088/products?_expand=productType&_sort=productTypeId")
+        .then(res => res.json())
+        .then(
+            (productData) => {
+                setProducts(productData)
+            }
+    )},
+    []
+    )
+
+
+    return(
+        <>
+            {products.map((product) => {
+                return <div key={`product--${product.id}`}>
+                <p>{product.name}</p>
+                <p>Type: {product.productType.name}</p>
+                <p>Price: {product.price}</p>
+                </div>
+            })}
+        </>
+    )
+}
