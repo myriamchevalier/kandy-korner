97e0fb46d6383e0acacf92b0c36c4b5c9238503d
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-09-02 11:17:49 -0500

    finished hiring form, added onChange events to track state, and an onClick to post new employee

--- src/employees/EmployeeForm.js
+++ src/employees/EmployeeForm.js
@@ -2,29 +2,25 @@ import { useEffect, useState } from "react"
 import { useHistory } from "react-router"
 // Form for user to hire employees
 // Create a form for hiring employees. You must be able to choose a location when filling out the form. An employee should have the following information provided.
-// Name (string)
-// Location (foreign key)
-// Manager (boolean) (i.e. Is this employee a manager or not?)
-// Full time (boolean)
-// Hourly rate (number)
-// address 
-// phone number
+
 // Once the user fills out the form, a new employee object representation should be created, then use fetch to POST that object to your database. When the saving process is complete, redirect the user to the /employee route to see a list of all employees.
 
 export const EmployeeHireForm = () => {
-    const [employee, updateEmployee] = useState([])
+    const [ employee, hireEmployee] = useState({
+        name: "",
+        address: "",
+        phoneNumber: "",
+        locationId: 0,
+        isManager: false,
+        isFullTime: false,
+        hourlyWage: 0
+
+    })
     const [employeeLocations, setEmployeeLocations] = useState([]) 
-    // const history = useHistory()
+    const history = useHistory()
     
     useEffect(
         () => {
-            fetch("http://localhost:8088/employees")
-            .then(res => res.json())
-            .then(
-                (employeeData) => {
-                    updateEmployee(employeeData)
-                }
-            )
             fetch("http://localhost:8088/locations") 
             .then(res => res.json())
             .then(
@@ -34,27 +30,33 @@ export const EmployeeHireForm = () => {
         },[]
     )
 
-    // const saveEmployee = (event) => {
-    //     event.preventDefault()
-    //     const newEmployee = {
-    //         name: employee.name,
-    //         specialty: employee.specialty
-    //     }
-    //     const fetchOptions = {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type" : "application/json"
-    //         }, 
-    //         body: JSON.stringify(newEmployee)
-    //     }
-    //     fetch("http://localhost:8088/employees", fetchOptions)
-    //     .then(res => res.json())
-    //     .then(
-    //         () => history.push("/employees")
-    //          )
-    // }
+    const saveNewEmployee = (event) => {
+        event.preventDefault()
+        const newEmployee = {
+            name: employee.name,
+            address: employee.address,
+            phoneNumber: employee.phoneNumber,
+            locationId: employee.locationId,
+            isManager: false,
+            isFullTime: false,
+            hourlyWage: employee.hourlyWage
+        }
+        const fetchOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(newEmployee)
+        }
 
-
+        fetch("http://localhost:8088/employees?_expand=location", fetchOptions)
+        .then(res => res.json())
+        .then(
+            () => {
+                history.push("/employees")
+            }
+        )
+    }
 
     return (
         <>
@@ -66,47 +68,102 @@ export const EmployeeHireForm = () => {
                         required autoFocus
                         type="text" 
                         className="form-control" 
-                        placeholder="Full name" />
+                        placeholder="Full name"
+                        onChange={
+                            (event) => {
+                                const copy = {...employee}
+                                copy.name = event.target.value
+                                hireEmployee(copy)
+                            }
+                        } />
                 </div>
                 <div className="form-group"> 
                     <label htmlFor="address">Address: </label>
                     <input 
+                        required
                         type="text" 
                         className="form-control" 
-                        placeholder="Street address" />
+                        placeholder="Street address"
+                        onChange={
+                            (event) => {
+                                const copy = {...employee}
+                                copy.address = event.target.value
+                                hireEmployee(copy)
+                            }
+                        } />
                 </div>
                 <div className="form-group"> 
                     <label htmlFor="phone">Phone number: </label>
                     <input 
+                        required
                         type="text" 
                         className="form-control" 
-                        placeholder="Phone number" />
+                        placeholder="000-000-0000"
+                        onChange={
+                            (event) => {
+                                const copy = {...employee}
+                                copy.phoneNumber = event.target.value
+                                hireEmployee(copy)
+                            }
+                        } />
                 </div>
                 <div className="form-group"> 
                     <label htmlFor="location">Location: </label>
-                    <select name="location" id="location">
+                    <select name="location" id="location"
+                    onChange={
+                        (event) => {
+                            const copy = {...employee}
+                            copy.locationId = parseInt(event.target.value)
+                            hireEmployee(copy)
+                        }
+                    }>
                         <option value="employeeLocation--0">Choose a location...</option>
                         {employeeLocations.map((employeeLocation) => 
-                        <option value={`location--${employeeLocation.id}`} key={`location--${employeeLocation.id}`}>{employeeLocation.name}</option>
+                        <option value={employeeLocation.id}
+                        >{employeeLocation.name}</option>
                         )}
                     </select>
                 </div>
                 <div className="form-group">
                     <label htmlFor="manager">Manager?</label>
-                    <input type="checkbox" id="manager"/>
+                    <input type="checkbox" id="manager"
+                    onChange={
+                        (event) => {
+                            const copy = {...employee}
+                            copy.isManager = event.target.checked
+                            hireEmployee(copy)
+                        }
+                    }/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="fullTime">Full-Time?</label>
-                    <input type="checkbox" id="fullTime"/>       
+                    <input type="checkbox" id="fullTime"
+                    onChange={
+                        (event) => {
+                            const copy = {...employee}
+                            copy.isFullTime = event.target.checked
+                            hireEmployee(copy)
+                        }
+                    }/>       
                 </div>
                 <div className="form-group">
                     <label htmlFor="hourlyWage">Hourly Wage</label>
-                    <input type="text" className="form-control" placeholder="0"/>
+                    <input 
+                    required
+                    type="text" className="form-control" placeholder="0"
+                    onChange={
+                        (event) => {
+                            const copy = {...employee}
+                            copy.hourlyWage = parseInt(event.target.value)
+                            hireEmployee(copy)
+                        }
+                    }/>
                 </div>
             </fieldset>
-        
-        
-        
+            <button className="button__hireForm"
+            onClick={saveNewEmployee}>
+                Hire Employee
+            </button>
         </>
     )
 }
